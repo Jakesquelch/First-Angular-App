90882328b2ca9d1fa4972f063cbecf4251996b07
Jake Squelch <jakewsquelch@gmail.com>
2025-09-28 14:59:29 +0100

    Added small change to website and added gitignore, node modules, and .angular

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  imports: [],
+  template: ` <h1>Hi Jake</h1> `,
+  styleUrls: ["./app.css"],
+})
+export class App {
+  title = "default";
+}
