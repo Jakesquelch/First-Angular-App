f3e03607933ca1d64998ba394210944465c1396c
Jake Squelch <jakewsquelch@gmail.com>
2025-09-28 15:19:21 +0100

    Small tweaks

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,9 +3,11 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   imports: [],
-  template: ` <h1>Hi Jake</h1> `,
+  template: ` 
+  <h1>Hi Jake!</h1>
+   `,
   styleUrls: ["./app.css"],
 })
 export class App {
-  title = "default";
+  title = "homes";
 }
