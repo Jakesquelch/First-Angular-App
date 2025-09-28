2a43dd7bb30d3928da0f57571ba64c6a8c2a0357
Jake Squelch <jakewsquelch@gmail.com>
2025-09-28 15:46:25 +0100

    Added code to home component and create a css file for home component

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styles: ``
+  styleUrls: ["./home.css"],
 })
-export class Home {
-
-}
+export class Home {}
